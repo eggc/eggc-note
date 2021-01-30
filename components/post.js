64b42385a45a77a22f11b54cb57c2b296f35ad62
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-01-30 17:15:59 +0900

    org を HTML に変換してからレンダリングする

--- components/post.js
+++ components/post.js
@@ -2,7 +2,7 @@ export default function Post({post}) {
   return (
     <div>
       <p>{post.title || "no title"}</p>
-      <p>{post.body || "no body"}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.body || "no body" }} />
     </div>
   )
 }
