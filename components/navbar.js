5a8abe2dab8fd18fd60260c8ac4d2bfd7f56e3f3
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-02-07 11:59:12 +0900

    プロフィール追加

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Navbar() {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -7,8 +9,8 @@ export default function Navbar() {
 
       <div className="navbar-menu">
         <div className="navbar-start">
-          <a className="navbar-item">Home</a>
-          <a className="navbar-item">Profile</a>
+          <Link href="/"><a className="navbar-item">Home</a></Link>
+          <Link href="/profile"><a className="navbar-item">Profile</a></Link>
           <a className="navbar-item">Memorandum</a>
         </div>
       </div>
