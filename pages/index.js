6b01137b8684e186430df26b0d350aace36dabce
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-02-07 11:46:33 +0900

    リンクを付ける

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Posts from '../components/posts'
 import getPostNames from '../lib/getPostNames'
+import Link from 'next/link'
 
 export default function Index({postNames}) {
   return (
     <ul>
-      {postNames.map((name, i) => { return <li key={i}>{name}</li> })}
+      {postNames.map((name, i) => { return <li key={i}><Link href="/posts">{name}</Link></li> })}
     </ul>
   )
 }
