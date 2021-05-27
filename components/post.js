bbb85b3d86ff9ccbb73101f74be3e81c479331d8
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-05-27 22:41:53 +0900

    日記専用のレイアウトをつけた

--- components/post.js
+++ components/post.js
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 
 export default function Post({post}) {
+  const extraClass = (post.title == '日記') ? 'eggc-diary' : ''
+
   return (
     <div>
       <div>
         <Link href="/">戻る</Link>
       </div>
       <hr />
-      <div className="eggc-post-container" dangerouslySetInnerHTML={{ __html: post.body }} />
+      <div className={`eggc-post-container ${extraClass}`} dangerouslySetInnerHTML={{ __html: post.body }} />
     </div>
   )
 }
