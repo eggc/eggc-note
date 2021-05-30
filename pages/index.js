b7615501e04fd8e6bac1c3c6f7c0424414f63064
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-05-30 10:05:36 +0900

    next/image を使う

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Page from '../components/page'
 import getSidebarItems from '../lib/getSidebarItems'
 
@@ -5,7 +6,7 @@ export default function Index({posts}) {
   return (
     <Page posts={posts}>
       <p>EGGC NOTE</p>
-      <img src="/img/top-image.jpg" />
+      <Image src="/img/top-image.jpg" width={1000} height={668} />
       <small>
         Photo by <a href="https://unsplash.com/@ashleywedwards?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ashley West Edwards</a> on <a href="https://unsplash.com/photos/wWZzXlDpMog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
       </small>
