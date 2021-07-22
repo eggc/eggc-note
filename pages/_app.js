8c3321b58f792fbbbb99c941b9b986dc1c65fd04
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-07-22 11:51:12 +0900

    アプリケーション名を変える

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <title>Eggc.diary</title>
+        <title>EGGC NOTE</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@500&display=swap" rel="stylesheet" />
