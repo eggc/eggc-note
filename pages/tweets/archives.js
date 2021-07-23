0d94cda1b3dbfed993b7a570d6701839c80bd6fa
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-07-23 16:42:35 +0900

    メンションとリツイートは消す

--- pages/tweets/archives.js
+++ pages/tweets/archives.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Autolinker from 'autolinker'
 import Page from '../../components/page'
 import getSidebarItems from '../../lib/getSidebarItems'
@@ -5,13 +6,11 @@ import rawTweets from '../../lib/raw/tweet'
 
 function renderTweet(tweet) {
   const date = new Date(tweet.created_at)
+
   return (
-    <div className={`row gy-4 ${tweet.user ? 'retweet' : ''}`} key={tweet.id}>
-      <div className="col-8">
+    <div className={`row`} key={tweet.id}>
+      <div className="col-10">
       <span dangerouslySetInnerHTML={{ __html: Autolinker.link(tweet.text)}}></span>
-      </div>
-      <div className="col-2">
-        <span className="text-muted">{tweet.user}</span>
       </div>
       <div className="col-2">
         <span className="text-muted">{date.toLocaleDateString() + " " + date.toLocaleTimeString()}</span>
@@ -21,10 +20,12 @@ function renderTweet(tweet) {
 }
 
 export default function Index(props) {
+  const tweets = props.tweets
+
   return (
     <Page {...props}>
       <h1 className="page-title">Twitter Archives</h1>
-      {props.tweets.map(renderTweet)}
+      {tweets.map(renderTweet)}
     </Page>
   )
 }
@@ -32,20 +33,20 @@ export default function Index(props) {
 export async function getStaticProps() {
   const tree = await getSidebarItems()
   const tweets = rawTweets.filter((data) => {
-    // メンションは消す
-    return !data.tweet.full_text.startsWith("@")
+    // メンションとリツイートは消す
+    return !data.tweet.full_text.startsWith("@") && !data.tweet.full_text.startsWith("RT @")
   }).map((data, i) => {
     const tweet = data.tweet
-    const matches = tweet.full_text.match("RT (@.*): ")
-    const text = matches ? tweet.full_text.slice(matches[0].length) : tweet.full_text
+    const text = tweet.full_text
+    const date = new Date(tweet.created_at)
 
     return {
       id: i,
-      user: matches ? matches[1] : '',
       text: text,
-      created_at: tweet.created_at
+      created_at: tweet.created_at,
+      created_at_int: date.getTime()
     }
-  })
+  }).sort((a, b) => a.created_at_int - b.created_at_int)
 
   return {
     props: { tree, tweets }
