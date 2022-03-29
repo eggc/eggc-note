3dc5aa7e89f04110491012626d4d5a8fe6a4a15e
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2022-03-29 17:06:23 +0900

    ✨ org を HTML に変換してから描画する

--- pages/memo/index.tsx
+++ pages/memo/index.tsx
@@ -1,19 +1,17 @@
 import Page from 'components/page'
 import {useState} from 'react'
 
-import Entry from '@lib/diary/Entry'
-import EntriesBuilder from '@lib/diary/EntriesBuilder'
+import HTMLEntriesBuilder, {HTMLEntry} from '@lib/diary/HTMLEntriesBuilder'
 
 type Props = {
   entries: string
 }
 
-
 export default function Index(props: Props) {
-  const entries = JSON.parse(props.entries)
+  const entries: HTMLEntry[] = JSON.parse(props.entries)
   const [currentEntry, setCurrentEntry] = useState(entries[0])
 
-  const renderEntries = (entry: Entry) => {
+  const renderEntries = (entry: HTMLEntry) => {
     return (
       <li key={entry.path}>
         <button type='button' className="btn btn-link" onClick={ ()=> setCurrentEntry(entry) }>
@@ -26,15 +24,21 @@ export default function Index(props: Props) {
   return (
     <Page {...props}>
       <div className="row">
-        <div className="col-sm-3">{entries.map(renderEntries)}</div>
-        <div className="col-sm-9">{currentEntry.raw}</div>
+        <div className="col-sm-3">
+          <ul>
+            {entries.map(renderEntries)}
+          </ul>
+        </div>
+        <div className="col-sm-9">
+          <span dangerouslySetInnerHTML={{ __html: currentEntry.html }}></span>
+        </div>
       </div>
     </Page>
   )
 }
 
 export async function getStaticProps() {
-  const entries: Entry[] = await EntriesBuilder.build()
+  const entries: HTMLEntry[] = await HTMLEntriesBuilder.build()
 
   return {
     props: { entries: JSON.stringify(entries) }
