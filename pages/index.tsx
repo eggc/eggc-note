82e1cdd643e20ed55cac260351c935808ccc7b96
eggc <no.eggchicken@gmail.com>
2022-04-09 22:56:31 +0900

    ♻ index.jsx -> index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Page from 'components/Page'
 
-export default function Index(props) {
+export default function Index(props: any) {
   return (
     <Page {...props}>
       <ul>
