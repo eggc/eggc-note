ce2d74adf8f4e48cf2e57ca6f6e0c7106326fc98
eggc <no.eggchicken@gmail.com>
2022-04-09 22:43:03 +0900

    ♻ _app.js -> _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,17 @@
-import '../styles/globals.scss'
+import 'styles/globals.scss'
 import 'prismjs/themes/prism-tomorrow.css';
-
-import { useEffect, Fragment } from 'react'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Fragment>
+    <>
       <Head>
         <title>EGGC NOTE</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
-    </Fragment>
+    </>
   )
 }
 
