87ae10ef1dd7d70a6608d5ce5e0df6edac8592a7
eggc <no.eggchicken@gmail.com>
2022-05-05 09:00:15 +0900

    📦 yarn upgrade --latest orga reorg-rehype @types/jest jest ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,5 @@
-// https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping
-
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig')
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
