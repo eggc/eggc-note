8a44d85923c8a8e0b2312c1ae9f6dcb4ad171c07
eggc <no.eggchicken@gmail.com>
2022-08-29 22:46:47 +0900

    ✨ feat: Remove sidebar from the top page

--- components/Page.tsx
+++ components/Page.tsx
@@ -10,15 +10,26 @@ import Col from 'react-bootstrap/Col'
 export type PageProps = {
   appTitle: string
   sidebarItems?: NavItemProps[]
+  noSidebar?: boolean
   children?: any
 }
 
 export default function Page(props: PageProps) {
+  let sidebar
+
+  if(props.noSidebar) {
+    sidebar = null
+  } else if(props.sidebarItems) {
+    sidebar = <Sidebar items={props.sidebarItems}></Sidebar>
+  } else {
+    sidebar = <AppSidebar></AppSidebar>
+  }
+
   return <>
     <Header title={props.appTitle}></Header>
     <Container fluid>
       <Row>
-        <Col style={{ flex: "0 0 200px"}}>
+        <Col style={{ flex: "0 0 200px"}} hidden={sidebar == null}>
           {sidebar}
         </Col>
         <Col>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Page from 'components/Page'
 
 export default function Index(props: any) {
+  const pageProps = Object.assign({}, props, { noSidebar: true })
+
   return (
-    <Page {...props}>
+    <Page {...pageProps}>
     </Page>
   )
 }
